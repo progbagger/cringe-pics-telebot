c2295103a5772b49c962377c3245950f38efac6a
Danil Astakhov <75833312+progbagger@users.noreply.github.com>
2026-08-11 23:47:11 +0700

    logging-levels: add log level selection on startup

--- src/cringe_pics_telebot/cli.py
+++ src/cringe_pics_telebot/cli.py
@@ -1,8 +1,19 @@
 import asyncio
+import logging
+import os
 
 import click
 
 from cringe_pics_telebot.bot.polling import start_polling
+
+log_level_name = os.environ.get("LOG_LEVEL_NAME", "INFO").upper()
+try:
+    log_level = logging.getLevelNamesMapping()[log_level_name]
+except KeyError:
+    logging.exception("Failed to set log level %s", log_level_name)
+    log_level = logging.INFO
+
+logging.getLogger().setLevel(log_level)
 
 
 @click.command()
