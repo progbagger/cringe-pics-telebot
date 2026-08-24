e92ceda5c590e8a22503d0ffa3b772e8dd108d49
Danil Astakhov <75833312+progbagger@users.noreply.github.com>
2026-08-24 22:17:17 +0700

    logging-basic-config: add logging.basicConfig to initialize logging system

--- src/cringe_pics_telebot/cli.py
+++ src/cringe_pics_telebot/cli.py
@@ -6,6 +6,8 @@ import click
 
 from cringe_pics_telebot.bot.polling import start_polling
 
+logging.basicConfig(level=logging.INFO)
+
 log_level_name = os.environ.get("LOG_LEVEL_NAME", "INFO").upper()
 try:
     log_level = logging.getLevelNamesMapping()[log_level_name]
